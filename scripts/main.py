a31daed9e062724aa4fb7ad6e078c6fa6cc3d48c
Domen Kovacic <domen.kovacic01@gmail.com>
2023-05-11 18:02:10 +0200

    Minor tweaks there and there.

--- scripts/main.py
+++ scripts/main.py
@@ -6,18 +6,38 @@ from nis import match
 
 
 
-todos = []
+# todos = []
 
 while True:
     user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             todos.append(todo)
+            
+            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+            
         case 'show':
-            for index, i in enumerate(todos):
+            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r')
+            todos= file.readlines()
+            file.close()
+            
+            # new_todos = []
+            
+            # for item in todos:
+            #     new_item = item.strip()
+            #     new_todos.append(new_item)
                 
+            new_todos = [item.strip('\n') for item in todos]   # Does same thing as above grayed-out for loop.
+            
+            for index, i in enumerate(new_todos):
+                item = item.strip('\n')
                 row = (f"{index + 1} - {i}")
                 print(row)
         case "edit":

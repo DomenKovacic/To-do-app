2fd5d709dfa7740c49316caa1d13dcac32b6e97a
Domen Kovacic <domen.kovacic01@gmail.com>
2023-05-15 16:52:04 +0200

    Made code more readable

--- scripts/main.py
+++ scripts/main.py
@@ -13,51 +13,72 @@ while True:
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ") + "\n"
-            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-            todos.append(todo)
+            todo = input("Enter a todo: ") + "\n"         
+                        
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r') as file:
+                todos = file.readlines()
             
-            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            todos.append(todo)
+                
+            
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w') as file:
+                file.writelines(todos)
+            
             
         case 'show':
-            file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r')
-            todos= file.readlines()
-            file.close()
-            
-            # new_todos = []
-            
-            # for item in todos:
-            #     new_item = item.strip()
-            #     new_todos.append(new_item)
+           
+           
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r') as file:
+                todos = file.readlines()
+        
                 
-            new_todos = [item.strip('\n') for item in todos]   # Does same thing as above grayed-out for loop.
+            new_todos = [item.strip('\n') for item in todos]   
             
-            for index, i in enumerate(new_todos):
+            for index, item in enumerate(new_todos):
                 item = item.strip('\n')
-                row = (f"{index + 1} - {i}")
+                row = (f"{index + 1} - {item}")
                 print(row)
         case "edit":
             number = int(input("Wich word would you like to edit?"))
             number = number - 1
+            
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r') as file:
+                 todos = file.readlines()
+            
             new_todo = input("How should we rename this?")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+            
+            
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w') as file:
+                file.writelines(todos)
+                
         case "complete":
             number = int(input("Wich word would you like to complete?"))
-            todos.pop(number - 1)
             
             
-                    
-              
-           
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'r') as file:
+                file.readlines(todos)
+                index = number - 1 
+                removed_todo = todos[index].strip('\n')             
+                todos.pop(index)
+            
+            
+            with open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w') as file:
+                file.writelines(todos)
+                
+            
+            message = f"Todo {removed_todo} was removed from the list."
+            print(message)
         
-    
         
+            
+                                                            
         case 'exit':
-            break
+           file = open('/home/domen/Desktop/To-do-app/scripts/todos.txt', 'w')
+           file.close()
+           break
+                
+            
         
     
     
